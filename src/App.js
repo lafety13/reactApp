88ea33134de2a76fedebaa2123063951b32905eb
vadim <kozak13vadim13@bigmir.net>
2017-10-06 14:28:41 +0300

    added a category list

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         super(props);
         this.state = {
             data: [],
-            categoryProduct: '',
+            categoryProduct: 'All',
             pageOfItems: [],
             search : '',
             activePage: 1,
@@ -32,63 +32,65 @@ class App extends Component {
 
     updateSearch (event) {
         this.setState({
+            activePage: 1,
             search : event.target.value.substr(0, 20)
         })
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-
-    handleChange(event) {
-        this.setState({search: event.target.value});
+    onClickCategory(event) {
+        this.setState({
+            categoryProduct: event.target.value
+        });
     }
 
     render() {
+
         let filteredProduct = this.state.data.filter((product) => {
                 return product.name.indexOf(this.state.search) !== -1
             }
         );
+        //let test = filteredProduct.filter(product => product.bsr_category === 'Amazon Launchpad');
+        //console.log(test);
+
         const page = this.state.activePage;
+
+
+
         const perPage = 10;
         const pages = Math.ceil(filteredProduct.length / perPage);
         const startOffset = (page - 1) * perPage;
         let startCount = 0;
 
 
-
         return (
-        <div className="App">
+        <div className={"container"}>
+            <div className={"row"}>
+                <div className="col-md-2">
+                    <div className="list-group">
+                        <a href="#" className="list-group-item active" >All</a>
+                        <a href="#" className="list-group-item list-group-item-action" onClick={this.onClickCategory.bind(this)}>Amazon Launchpad</a>
+                    </div>
+                </div>
 
-            <form onSubmit={this.handleSubmit.bind(this)}>
-                <label>
-                    Pick your favorite La Croix flavor:
-                    <select value={this.state.categoryProduct} onChange={this.handleChange.bind(this)}>
-                        <option value="grapefruit">Grapefruit</option>
-                        <option value="lime">Lime</option>
-                        <option value="coconut">Coconut</option>
-                        <option value="mango">Mango</option>
-                    </select>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+                <div className="col-md-8">
+                    <Input
+                        value = {this.state.search}
+                        onChange = {this.updateSearch.bind(this)}/>
 
+                    {filteredProduct.map((product, index) => {
+                        if (index >= startOffset && startCount < perPage) {
+                            startCount++;
+                            return (
+                                <div key={index}>{product.name}</div>
+                            );
+                        }
+                    })}
 
-            <Input
-                value = {this.state.search}
-                onChange = {this.updateSearch.bind(this)}/>
+                    <Pagination className="users-pagination pull-right" bsSize="medium" maxButtons={10} first last next
+                                prev boundaryLinks items={pages} activePage={page} onSelect={this.onChangePage.bind(this)}/>
+                </div>
 
-            {filteredProduct.map((product, index) => {
-                if (index >= startOffset && startCount < perPage) {
-                    startCount++;
-                    return (
-                        <div key={index}>{product.name}</div>
-                    );
-                }
-            })}
-
-            <Pagination className="users-pagination pull-right" bsSize="medium" maxButtons={10} first last next
-                        prev boundaryLinks items={pages} activePage={page} onSelect={this.onChangePage.bind(this)}/>
+            </div>
         </div>
         );
     }
